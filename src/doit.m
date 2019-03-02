5a071d8cd85796ecb75b44fa4091a5d0908a4c61
Andreas Weber <octave@josoansi.de>
2019-03-02 09:16:24 +0100

    Add a "samples" dir for videos

--- src/doit.m
+++ src/doit.m
@@ -2,7 +2,7 @@ autoload ("__cap_open__", which ("cap_ffmpeg_wrapper.oct"));
 autoload ("__cap_grab_frame__", which ("cap_ffmpeg_wrapper.oct"))
 autoload ("__cap_retrieve_frame__", which ("cap_ffmpeg_wrapper.oct"));
 
-x = __cap_open__ ("vtest.avi");
+x = __cap_open__ ("../samples/vtest.avi");
 #x = __cap_open__ ("dge.mp4");
 
 im = [];

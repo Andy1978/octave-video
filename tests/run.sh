be15b96eb6ce8dcdbfbe1218b0a0f4dcf7563969
Andreas Weber <octave@josoansi.de>
2019-10-31 22:52:14 +0100

    tests/run.sh: add FFmpeg versions to output

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-OCMD='pkg load video; system ("head -n2 /etc/os-release"); printf ("OCTAVE_VERSION = %s\n", OCTAVE_VERSION); test VideoWriter; test VideoReader; printf ("---\n");'
+OCMD='pkg load video; system ("head -n2 /etc/os-release"); printf ("OCTAVE_VERSION = %s\n", OCTAVE_VERSION); x=VideoWriter("foo.avi"); printf ("FFmpeg_versions = %s\n", x.FFmpeg_versions); test VideoWriter; test VideoReader; printf ("---\n");'
 
 docker run -it debian_stretch_octave_4_4_1 octave -q --eval "$OCMD"
 

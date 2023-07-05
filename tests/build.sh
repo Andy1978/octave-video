5670ae31782f3339b34438f0ea1994ed70122604
Andreas Weber <octave@josoansi.de>
2023-07-05 11:03:44 +0200

    Updated docker tests, removed no longer supported distributions

--- tests/build.sh
+++ tests/build.sh
@@ -2,20 +2,31 @@
 
 cp ../video-2*.tar.gz .
 
-## Debian stretch
-docker build -f Dockerfile_debian_stretch_octave_4_4_1 -t debian_stretch_octave_4_4_1 .
-docker build -f Dockerfile_debian_stretch_octave_5_1_0 -t debian_stretch_octave_5_1_0 .
+## Debian buster
+docker build -f Dockerfile_debian_buster -t debian_buster .
 
-# Debian stretch, libav 12.3
-docker build -f Dockerfile_debian_stretch_octave_4_4_1_libav_12.3 -t debian_stretch_octave_4_4_1_libav_12.3 .
+## Debian bullseye
+docker build -f Dockerfile_debian_bullseye -t debian_bullseye .
 
-## CentOS 7
-docker build -f Dockerfile_centos7_epel_ffmpeg -t centos7_epel_ffmpeg .
+## Debian bookworm
+docker build -f Dockerfile_debian_bookworm -t debian_bookworm .
 
 ## Fedora 31
 docker build -f Dockerfile_fedora31_ffmpeg -t fedora31_ffmpeg .
 
+## Fedora 33
+docker build -f Dockerfile_fedora33_ffmpeg -t fedora33_ffmpeg .
+
+## Fedora 38
+docker build -f Dockerfile_fedora38_ffmpeg -t fedora38_ffmpeg .
+
 ## Ubuntu 20.04
 docker build -f Dockerfile_ubuntu_20_04 -t ubuntu_20_04 .
+
+## Ubuntu 22.04
+docker build -f Dockerfile_ubuntu_22_04 -t ubuntu_22_04 .
+
+## Ubuntu 23.04
+docker build -f Dockerfile_ubuntu_23_04 -t ubuntu_23_04 .
 
 rm -f video*.tar.gz
--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -e
 
+# INFO: "test VideoWriter" writes and reads a video
 OCMD='pkg load video; system ("head -n2 /etc/os-release"); printf ("OCTAVE_VERSION = %s\n", OCTAVE_VERSION); x=VideoWriter("foo.avi"); printf ("FFmpeg_versions = %s\n", x.FFmpeg_versions); test VideoWriter; printf ("---\n");'
+echo "octave command: ${OCMD}" | tee run.log
 
-docker run -it debian_stretch_octave_4_4_1 octave -q --eval "$OCMD"
+docker run -it debian_buster octave -q --eval "$OCMD" | tee -a run.log
 
-docker run -it debian_stretch_octave_5_1_0 octave -q --eval "$OCMD"
+docker run -it debian_bullseye octave -q --eval "$OCMD"
 
-docker run -it centos7_epel_ffmpeg octave -q --eval "$OCMD"
+docker run -it debian_bookworm octave -q --eval "$OCMD"
 
 docker run -it fedora31_ffmpeg octave -q --eval "$OCMD"
 
-docker run -it debian_stretch_octave_4_4_1_libav_12.3 octave -q --eval "$OCMD"
+docker run -it fedora33_ffmpeg octave -q --eval "$OCMD"
+
+docker run -it fedora38_ffmpeg octave -q --eval "$OCMD"
 
 docker run -it ubuntu_20_04 octave -q --eval "$OCMD"
+
+docker run -it ubuntu_22_04 octave -q --eval "$OCMD"
+
+docker run -it ubuntu_23_04 octave -q --eval "$OCMD"

2dd679dba574b8c87b0b1a8806383f43b9073910
Andreas Weber <andy@josoansi.de>
2023-07-15 15:48:44 +0200

    update playground/standalone

--- playground/standalone/standalone.cc
+++ playground/standalone/standalone.cc
@@ -1,5 +1,6 @@
 /*
  * Playground to test implementation in cap_ffmpeg_impl_ov.hpp
+ * (Not using functions in cap_ffmpeg_wrapper.cc)
  */
 
 #include <iostream>
@@ -7,55 +8,73 @@
 
 #include "cap_ffmpeg_impl_ov.hpp"
 
-
-int
-main (void)
+int main (void)
 {
-  set_verbosity_level (3);
-  
-  std::string filename = "foo.mov";
+  set_verbosity_level (1); // errors, warnings, info and verbose debug messages (but not maximum = 4)
+  std::string filename = "foo.mp4";
 
-  // codec tag, in OpenCV "fourcc" is used interchangeably
-  // empty fourcc selects default codec_id for guessed container
-  unsigned int tag;
-  //std::string fourcc = "MPEG";
-  //tag = MKTAG(fourcc[0], fourcc[1], fourcc[2], fourcc[3]);
-
-  const AVOutputFormat* fmt = av_guess_format	(NULL, filename.c_str (), NULL);
-  if (! fmt)
-		error ("Can't guess container format from filename '%s'", filename.c_str ());
-  
-  if (0 && fmt && fmt->codec_tag)
-    {
-      const AVCodecTag * ptags = fmt->codec_tag[0];
-      while (ptags->id != AV_CODEC_ID_NONE)
-        {
-          unsigned int tag = ptags->tag;
-          printf("fourcc tag 0x%08x/'%c%c%c%c' codec_id %04X\n", tag, CV_TAG_TO_PRINTABLE_CHAR4(tag), ptags->id);
-          ptags++;
-        }
-    }
-
-  // guess codec
-  //AVCodecID x = av_guess_codec (fmt, NULL, filename.c_str ()
-
-  tag = av_codec_get_tag (fmt->codec_tag, fmt->video_codec);
-
-  printf("fourcc tag 0x%08x/'%c%c%c%c'\n", tag, CV_TAG_TO_PRINTABLE_CHAR4(tag));
-
-  CvVideoWriter_FFMPEG *h = new CvVideoWriter_FFMPEG ();
+  CvVideoWriter_FFMPEG *w = new CvVideoWriter_FFMPEG ();
 
   double fps   = 30;
-  int width    = 25;
-  int height   = 15;
+  int width    = 250;
+  int height   = 150;
+  int cn       = 3;
   bool isColor = 1;
-  
-  bool valid = h->open (filename.c_str (), tag, fps, width, height, isColor);
+
+  const AVOutputFormat* fmt = av_guess_format	(NULL, filename.c_str (), NULL);
+  unsigned int tag = av_codec_get_tag (fmt->codec_tag, fmt->video_codec);
+  printf("DEBUG: tag = '%c%c%c%c'\n", CV_TAG_TO_PRINTABLE_CHAR4(tag));
+
+  bool valid = w->open (filename.c_str (), tag, fps, width, height, isColor);
   if (!valid)
-    {
-      // FIXME: CvVideoWriter_FFMPEG::open just returns false without explanation why
-      error ("Opening '%s' for writing failed", filename.c_str ());
-    }
-    
+    std::cerr << "Opening '" << filename << "' for writing failed" << std::endl;
+
+  uint8NDArray f = uint8NDArray (dim_vector (height, width, cn));
+
+  int step = width * cn;
+  int origin = 0;
+
+  printf ("DEBUG: width=%i, height=%i, cn=%i, step=%i\n", width, height, cn, step);
+
+  Array<octave_idx_type> perm (dim_vector (3, 1));
+  perm(0) = 2;
+  perm(1) = 1;
+  perm(2) = 0;
+  f = f.permute (perm);
+
+  unsigned char *t = reinterpret_cast<unsigned char*>(f.fortran_vec());
+  for (int x = 0; x < 300; ++x)
+  {
+    for (int i = 0; i < width*height*cn; ++i)
+      t[i] = i + 5*x;
+
+    bool ret = w->writeFrame (t, step, width, height, cn, origin);
+    if (! ret)
+      std::cerr << "CvVideoWriter_FFMPEG::writeFrame failed" << std::endl;
+  }
+
+  w->close();
+
+  CvCapture_FFMPEG *r = new CvCapture_FFMPEG ();
+  valid = r->open (filename.c_str ());
+  if (!valid)
+    std::cerr << "Opening '" << filename << "' for reading failed" << std::endl;
+
+  int frames_read = 0;
+  while (r->grabFrame ())
+  {
+      unsigned char* data;
+      int depth;        // 0 = CV_8U, 1 glaube ich signed, 2 = CV_16U
+
+      bool ret = r->retrieveFrame (0, &data, &step, &width, &height, &cn, &depth);
+      printf ("ret = %i, width = %i, height = %i, step = %i, cn = %i, depth = %i\n", ret, width, height, step, cn, depth);
+
+      assert (cn == 3);
+      frames_read++;
+  }
+
+  printf ("frames_read = %i\n", frames_read);
+  r->close ();
+
   return 0;
 }
